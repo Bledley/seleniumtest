edda03b6e78b2875873743bddc8fc8edd4a34f5b
Bledley <bledley123@gmail.com>
2019-01-26 22:33:32 +0200

    returned main changes

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -8,6 +8,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.byText;
 
 public class HomePage {
-
+    //public SelenideElement emailField = $(xpath("//span[@class='wpcf7-form-control-wrap your-email']"));
+    public SelenideElement emailField = $(xpath("//input[@type='email']"));
     public SelenideElement submit = $(xpath("//input[@class='wpcf7-form-control wpcf7-submit']"));
 }
